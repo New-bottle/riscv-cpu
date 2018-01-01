6ef93f575d326f6a4f82c4b88a3257321a8c7172
New-bottle <775712558@qq.com>
2018-01-01 21:10:48 +0800

    logic & shift ok

--- code/ex.v
+++ code/ex.v
@@ -16,18 +16,73 @@ module ex (
 );
 
 	reg[`RegBus]            logicout;
+	reg[`RegBus]            arithout;
+	reg[`RegBus]            shiftres;
 
 
+	// logic
 	always @ (*) begin
 		if (rst == `RstEnable) begin
 			logicout <= `ZeroWord;
 		end else begin
 			case (aluop_i)
-				`EXE_OR_OP:begin
+				`EXE_ANDI_OP, `EXE_AND_OP:begin
+					logicout <= reg1_i & reg2_i;
+				end
+				`EXE_ORI_OP, `EXE_OR_OP:begin
 					logicout <= reg1_i | reg2_i;
+				end
+				`EXE_XORI_OP, `EXE_XOR_OP:begin
+					logicout <= reg1_i ^ reg2_i;
 				end
 				default:   begin
 					logicout <= `ZeroWord;
+				end
+			endcase
+		end
+	end
+
+	// arithmetic
+	reg [`RegBus] reg2_i_mux;
+	reg [`RegBus] result_sum;
+	assign reg2_i_mux = ((sel2_i == `FUNCT7_SUB) || 
+						 (aluop_i == `FUNCT3_SLT)) ? 
+						 (~reg2_i) + 1 : reg2_i;
+	assign result_sum = reg1_i + reg2_i_mux;
+	assign 
+	always @ (*) begin
+		if (rst == `RstEnable) begin
+			arithout <= `ZeroWord;
+		end else begin
+			case (aluop_i)
+				`FUNCT3_ADDI:begin
+					arithout <= reg1_i + reg2_i;
+				end
+				`FUNCT3_SLLI:begin
+					arithout <= reg1_i << reg2_i;
+				end
+				`FUNCT3_SLTI:begin
+				end
+	end
+
+	// shift
+	always @ (*) begin 
+		if (rst == `RstEnable) begin
+			shiftres <= `ZeroWord;
+		end else begin
+			case (aluop_i)
+				`EXE_SLLI_OP, `EXE_SLL_OP:begin
+					shiftres <= reg1_i << reg2_i[4:0];
+				end
+				`EXE_SRLI_OP, `EXE_SRL_OP:begin
+					shiftres <= reg1_o >> reg2_i[4:0];
+				end
+				`EXE_SRAI_OP, `EXE_SRA_OP:begin
+					shiftres <= ({32{reg2_i[31]}}<<(6'd32-{1'b0,reg1_i[5:0]}))
+								| reg2_i >> reg1_i[4:0];
+				end
+				default:begin
+					shiftres <= `ZeroWord;
 				end
 			endcase
 		end
@@ -37,11 +92,11 @@ module ex (
 		wd_o <= wd_i;
 		wreg_o <= wreg_i;
 		case (alusel_i)
-			`EXE_RES_LOGIC: begin
-				wdata_o <= logicout;
+			`EXE_:begin
+				wdata_o <= 
 			end
-			`OP_OP_IMM: begin
-				s
+			`OP_OP: begin
+				wdata_o <= logicout;
 			end
 			default: begin
 				wdata_o <= `ZeroWord;
