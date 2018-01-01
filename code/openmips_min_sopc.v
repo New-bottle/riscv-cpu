981e9750b6410181058c90385558d7bd400f54dc
New-bottle <775712558@qq.com>
2018-01-02 00:33:26 +0800

    解决小端序的问题

--- code/openmips_min_sopc.v
+++ code/openmips_min_sopc.v
@@ -8,17 +8,28 @@ module openmips_min_sopc (
 );
 
 	wire [`InstAddrBus] inst_addr;
-	wire [`InstBus]     inst;
+	wire [`InstBus]     inst_o, inst_i;
 	wire                rom_ce;
+
+	assign inst_i[7:0] = inst_o[31:24];
+	assign inst_i[15:8] = inst_o[23:16];
+	assign inst_i[23:16] = inst_o[15:8];
+	assign inst_i[31:24] = inst_o[7:0];
+	/*
+	assign inst_i[`InstBus/4*1-1:`InstBus/4*0] = inst_o[`InstBus/4*4-1:`InstBus/4*3];
+	assign inst_i[`InstBus/4*2-1:`InstBus/4*1] = inst_o[`InstBus/4*3-1:`InstBus/4*2];
+	assign inst_i[`InstBus/4*3-1:`InstBus/4*2] = inst_o[`InstBus/4*2-1:`InstBus/4*1];
+	assign inst_i[`InstBus/4*4-1:`InstBus/4*3] = inst_o[`InstBus/4*1-1:`InstBus/4*0];
+	*/
 
 	openmips openmips0 (
 		.clk(clk), .rst(rst),
-		.rom_addr_o(inst_addr), .rom_data_i(inst),
+		.rom_addr_o(inst_addr), .rom_data_i(inst_i),
 		.rom_ce_o(rom_ce)
 	);
 
 	inst_rom inst_rom0 (
 		.ce(rom_ce),
-		.addr(inst_addr), .inst(inst)
+		.addr(inst_addr), .inst(inst_o)
 	);
 endmodule
